667391b40c1eb5f5fe7c2f546a4664676f0a420e
hasnah98 <hasnah.ah98@gmail.com>
2025-12-24 01:24:12 +0300

    Add Models

---
+++ StudentCRUD/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCRUD.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, Range(1, 120)]
+        public int Age { get; set; }
+    }
+}
